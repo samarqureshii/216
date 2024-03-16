e591a57c76b7b87032775aeb35e5531a2985171f
samarqureshii <samarq713@gmail.com>
2024-03-16 11:50:04 -0400

    Finished lab 3

--- lab3/ADSR_env.m
+++ lab3/ADSR_env.m
@@ -1,52 +1,44 @@
 function [ Y ] = ADSR_env( Z, beat_length, num_beats, fs )
-%ADSR_env uses envelopes to produce a strummed version of Z
-%   given a signal Z, the beat length in seconds, the number of beats to be 
-%   covered by a single repetition of the strum pattern, and the sampling
-%   frequency of the original signal, this function outputs Y, a strummed
-%   version of Z
+    n = floor(fs*beat_length*num_beats);
+    E = zeros(n,1);
 
-%n is the number of samples per repetition of the strum pattern
-n = floor(fs*beat_length*num_beats);
-%E will be our envelope for one strum pattern
-E = zeros(n,1);
+    % modified strum pattern to match the song's rhythm
+    strums = [0, 1/6, 1/3, 1/2, 2/3, 5/6];
 
-%strums is a vector indicating what fraction of the way through the
-%envelope each strum will be performed
-strums = [0, 1/4, 11/16, 3/4, 15/16];
-%ADSR contains fractional lengths of attack, decay, sustain, and release segments
-ADSR = [0.1,0.4,0.4,0.1]; %entries here should sum to 1
-peak = 1; %peak volume
-mid = 0.2; %sustain volume
+    % updated ADSR lengths for a different envelope shape
+    ADSR = [0.05, 0.3, 0.5, 0.15]; % envelope should add up to 1
 
-A = 0;
-D = ADSR(1);
-S = D+ADSR(2);
-R = S+ADSR(3);
+    % new peak and sustain levels for different dynamics
+    peak = 0.8; % reduced peak for a softer attack
+    mid = 0.3;  % increased sustain for more emphasis
 
-ind = 0; %keeps track of which index of E the next strum will start from
-%each iteration of the for loop generates a single strum
-for i=1:length(strums)
-    if(i~=length(strums))
-        k = floor(n*strums(i+1))-ind;
-    else
-        k = n-ind;
+    A = 0;
+    D = ADSR(1);
+    S = D + ADSR(2);
+    R = S + ADSR(3);
+
+    ind = 0;
+    for i = 1:length(strums)
+        if (i ~= length(strums))
+            k = floor(n*strums(i+1)) - ind;
+        else
+            k = n - ind;
+        end
+        d = floor(D*k);
+        s = floor(S*k);
+        r = floor(R*k);
+        F = zeros(1,k);
+        F(1:d) = linspace(0, peak, d);
+        F(d+1:s) = linspace(peak, mid, s-d);
+        F(s+1:r) = linspace(mid, mid, r-s);
+        F(r+1:k) = linspace(mid, 0, k-r);
+        E(ind+1:ind+k) = F;
+        ind = ind + k;
     end
-    d = floor(D*k);
-    s = floor(S*k);
-    r = floor(R*k);
-    F = zeros(1,k); %F will be the envelope for a single strum
-    F(1:d) = linspace(0,peak,d); %attack
-    F(d+1:s) = linspace(peak,mid,s-d); %decay
-    F(s+1:r) = linspace(mid,mid,r-s); %sustain
-    F(r+1:k) = linspace(mid,0,k-r); %release
-    E(ind+1:ind+k) = F; %append each F to E
-    ind = ind + k;
+
+    E(ind+1:n) = E(ind);
+
+    reps = length(Z)/n;
+    E_rep = repmat(E, 1, ceil(reps));
+    Y = E_rep(1:length(Z)).*Z;
 end
-
-E(ind+1:n) = E(ind); %just to account for floor effects
-
-reps = length(Z)/n; %maximum number of strum pattern repetitions needed for the song
-E_rep = repmat(E,1,ceil(reps)); %repeat the strum pattern enough times
-Y = E_rep(1:length(Z)).*Z; %multiply E_rep with X, truncating E_rep to be the same length as X
-end
-
--- lab3/get_song_wave.m
+++ lab3/get_song_wave.m
@@ -9,10 +9,31 @@ n_chords = length(song); %number of chords that get played
 %each iteration, however, the code is simpler here - no calculation of song
 %length (in terms of samples) required
 Z = [];
-for i=1:n_chords
-    Z = [Z, get_chord_wave( ____ , ____ ,____)]; %fill in the blanks!
-        %appends each chord to Z
-end
+    for i=1:n_chords
+        Z = [Z, get_chord_wave( song{i,1}, song{i, 2}*beat_length , fs)]; %fill in the blanks!
+            %appends each chord to Z
+    end
+
+
+
+window_size = 256; 
+noverlap = []; 
+nfft = []; 
+
+% calculate the spectrogram
+[S, F, T, P] = spectrogram(Z, window_size, noverlap, nfft, fs);
+
+% display the spectrogram
+spectrogram(Z, window_size, noverlap, nfft, fs, 'yaxis');
+
+colormap('jet'); 
+colorbar; 
+
+% labels and title
+xlabel('Time (s)');
+ylabel('Frequency (Hz)');
+title('Spectrogram of the Signal');
+
 
 end
 
